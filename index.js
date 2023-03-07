d4517acc1808bdedbb9693a3ee37562eaebdb434
kevin344k <105673385+kevin344k@users.noreply.github.com>
2023-03-07 05:39:51 +0000

    2

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app=express()
 const PORT=process.env.PORT || 3000
 const path=require('path')
 
-
-app.use(express.static(__dirname,'public'))
-
 app.set('view engine','html')
+
+app.use(express.static(__dirname+'/public'))
+
+
 
 app.get('/',(req,res)=>{
   res.render('clase1-createPwd.html')
